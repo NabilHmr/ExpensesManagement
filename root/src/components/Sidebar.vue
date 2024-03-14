d922bfeb6a3b74abf795781d86899eaf42ba7fe1
Nabil <nhamri35@gmail.com>
2024-03-14 10:19:34 +0100

    improve sidebar code

--- root/src/components/Sidebar.vue
+++ root/src/components/Sidebar.vue
@@ -1,201 +1,68 @@
 <template>
-  <aside :class="`${is_expanded ? 'is-expanded' : ''}`">
-    <div class="logo">
-      <img :src="logoURL" alt="Vue" />
-    </div>
-
-    <div class="">
-<!--      <button class="menu-toggle" >-->
-      <bars-3-icon class="icon" @click="ToggleMenu"/>
-<!--      </button>-->
-    </div>
-
+  <aside>
     <h3>Menu</h3>
+    <hr>
     <div class="menu">
       <router-link to="/" class="button">
-        <home-icon class="icon"/>
-        <span class="material-icons">home</span>
+<!--        <home-icon class="icon"/>-->
+        <span>Home</span>
       </router-link>
-      <router-link to="/about" class="button">
-        <chart-pie-icon class="icon"/>
-        <span class="material-icons">Charts</span>
-      </router-link>
-<!--      <router-link to="/team" class="button">-->
-<!--        <span class="material-icons">group</span>-->
-<!--      </router-link>-->
-<!--      <router-link to="/contact" class="button">-->
-<!--        <span class="material-icons">email</span>-->
-<!--      </router-link>-->
     </div>
-
-    <div class="flex"></div>
-
     <div class="menu">
-      <router-link to="/settings" class="button">
-        <span class="material-icons">settings</span>
-        <span class="text">Settings</span>
+      <router-link to="/dashboard" class="button">
+<!--        <chart-pie-icon class="icon"/>-->
+        <span>Charts</span>
       </router-link>
     </div>
   </aside>
 </template>
 
 <script setup>
-import { ref } from 'vue'
-
-import { HomeIcon, ChartPieIcon, Bars3Icon} from '@heroicons/vue/24/solid';
-
-const is_expanded = ref(localStorage.getItem("is_expanded") === "true")
-
-const ToggleMenu = () => {
-  is_expanded.value = !is_expanded.value
-  localStorage.setItem("is_expanded", is_expanded.value)
-}
+// import { HomeIcon, ChartPieIcon } from '@heroicons/vue/24/solid';
 </script>
 
 <style lang="scss" scoped>
 aside {
   position: fixed;
   top: 0;
-  left: 0; /* Placer la sidebar tout à gauche */
+  left: 0;
   display: flex;
   flex-direction: column;
   background-color: darkgray;
   color: var(--light);
-  width: calc(2rem + 32px);
-  overflow: hidden;
-  height: 100vh; /* Occuper toute la hauteur de la fenêtre */
   padding: 1rem;
-  transition: 0.2s ease-in-out;
+  width: auto;
+  max-width: calc(100vw - 2rem);
+  height: 100vh; /* Hauteur à 100% de la fenêtre */
+}
 
-  .flex {
-    flex: 1 1 0%;
-  }
+h3 {
+  color: var(--grey);
+  font-size: 0.875rem;
+  margin-bottom: 0.5rem;
+  text-transform: uppercase;
+}
 
-  .logo {
-    margin-bottom: 1rem;
+.menu {
+  margin: 0 -1rem;
+}
 
-    img {
-      width: 2rem;
-    }
-  }
+.button {
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  padding: 0.5rem 1rem;
+  white-space: nowrap; /* Empêcher le texte de se couper */
+  overflow: hidden; /* Masquer le débordement du texte */
+  text-overflow: ellipsis; /* Afficher "..." si le texte dépasse */
+}
 
-  .menu-toggle-wrap {
-    display: flex;
-    justify-content: flex-end;
-    margin-bottom: 1rem;
+.material-icons {
+  font-size: 2rem;
+  color: var(--light);
+}
 
-    position: relative;
-    top: 0;
-    transition: 0.2s ease-in-out;
-
-    .menu-toggle {
-      transition: 0.2s ease-in-out;
-      .material-icons {
-        font-size: 2rem;
-        color: var(--light);
-        transition: 0.2s ease-out;
-      }
-
-      &:hover {
-        .material-icons {
-          color: var(--primary);
-          transform: translateX(0.5rem);
-        }
-      }
-    }
-  }
-
-  h3, .button .text {
-    opacity: 0;
-    transition: opacity 0.3s ease-in-out;
-  }
-
-  h3 {
-    color: var(--grey);
-    font-size: 0.875rem;
-    margin-bottom: 0.5rem;
-    text-transform: uppercase;
-  }
-
-  .menu {
-    margin: 0 -1rem;
-
-    .button {
-      display: flex;
-      align-items: center;
-      text-decoration: none;
-
-      transition: 0.2s ease-in-out;
-      padding: 0.5rem 1rem;
-
-      .material-icons {
-        font-size: 2rem;
-        color: var(--light);
-        transition: 0.2s ease-in-out;
-      }
-      .text {
-        color: var(--light);
-        transition: 0.2s ease-in-out;
-      }
-
-      &:hover {
-        background-color: var(--dark-alt);
-
-        .material-icons, .text {
-          color: var(--primary);
-        }
-      }
-
-      &.router-link-exact-active {
-        background-color: var(--dark-alt);
-        border-right: 5px solid var(--primary);
-
-        .material-icons, .text {
-          color: var(--primary);
-        }
-      }
-    }
-  }
-
-  .footer {
-    opacity: 0;
-    transition: opacity 0.3s ease-in-out;
-
-    p {
-      font-size: 0.875rem;
-      color: var(--grey);
-    }
-  }
-
-  &.is-expanded {
-    width: var(--sidebar-width);
-
-    .menu-toggle-wrap {
-      top: -3rem;
-
-      .menu-toggle {
-        transform: rotate(-180deg);
-      }
-    }
-
-    h3, .button .text {
-      opacity: 1;
-    }
-
-    .button {
-      .material-icons {
-        margin-right: 1rem;
-      }
-    }
-
-    .footer {
-      opacity: 0;
-    }
-  }
-
-  @media (max-width: 1024px) {
-    position: absolute;
-    z-index: 99;
-  }
+span {
+  color: var(--light);
 }
 </style>
